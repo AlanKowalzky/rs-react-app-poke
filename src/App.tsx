2be1ecc2cc685ba312d68c7e5cb52c913b2bedc6
AlanKowalzky <alan.kowalzky@gmail.com>
2025-07-14 11:00:02 +0200

    chore: aboout deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import Search from './components/Search';
 import CardList from './components/CardList';
 import Loader from './components/Loader';
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 
 interface CardProps {
   item: { name: string; url: string };
