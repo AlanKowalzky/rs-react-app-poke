61b70c8c140d0f2c6b43596a949acea75726446a
alan.kowalzky <alan.kowalzky@gmail.com>
2025-08-17 10:07:32 +0000

    feat before manual

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,11 +1,4 @@
-import type { Metadata } from 'next';
 import { ReactNode } from 'react';
-import { NextIntlClientProvider, useMessages } from 'next-intl';
-
-export const metadata: Metadata = {
-  title: 'Pokemon App (Migracja)',
-  description: 'A Next.js app to browse Pokemon (Migration in progress)',
-};
 
 export default async function RootLayout({
   children,
@@ -18,25 +11,10 @@ export default async function RootLayout({
   // Jawne odczytanie locale
   const locale = params.locale;
 
-  // Pobierz wiadomości do przekazania do ClientProvider
-  const messages = useMessages(); // Użyj useMessages w Server Component
-
   return (
     <html lang={locale}>
       <body>
-        {/* Opakowujemy zawartość body w NextIntlClientProvider */}
-        <NextIntlClientProvider locale={locale} messages={messages}>
-        <header style={{ backgroundColor: 'lightgray', padding: '10px' }}>
-          <h1>Nagłówek (Placeholder)</h1>
-          <p>Obecny język: {locale}</p>
-        </header>
-        <main style={{ padding: '20px' }}>
-          {children}
-        </main>
-        <footer style={{ backgroundColor: 'lightgray', padding: '10px', marginTop: '20px' }}>
-          <p>Stopka (Placeholder)</p>
-        </footer>
-        </NextIntlClientProvider>
+ {children}
       </body>
     </html>
   );
